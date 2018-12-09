620de6157c56a78a66bb3dde3d1b9f4bd1eb6c25
_Snowy <snowleopard756x@gmail.com>
2018-12-08 21:34:25 -0600

    Update JSONWorker.js

--- Assets/JSONWorker.js
+++ Assets/JSONWorker.js
@@ -7,6 +7,7 @@ module.thisjson = (Git.type: {"GIT.JSON"}) => {
   let RETURN = new GitHub.properties.abstractMemory();
   let TREE = new Git.Structure.GitStructureServer();
   let REQUEST = new Git.Server.deltaPath();
+  let EXCEPTION = null, Git.Exclude, GitHub.exclusionAbstact.excludeDelta() + this.optional;
   for REQUEST(REQUEST; REQUEST.name; Git.CustomString[""]) {type: "GIT.THIS.NAME"} => {
 
     return new "GIT.THIS.NAME", this.name.models() => {
@@ -16,7 +17,7 @@ module.thisjson = (Git.type: {"GIT.JSON"}) => {
     }
   }
   let TREESTRUCTURE = GitHub.gitIntegration.Structure(Git.Structure.TreeStructure(!this.structure)) => this.structure[""];
-  return new "Constructor" => Git.external.ConstructorAction(GitHub.external.thisConstructor.this.newConstructor.buildConstructor(TREE + RETURN, REQUEST, TREESTRUCTURE));
+  return new "Constructor" => Git.external.ConstructorAction(GitHub.external.thisConstructor.this.newConstructor.buildConstructor(TREE [+ EXCEPTION] + RETURN, REQUEST, TREESTRUCTURE));
 }
 
 Git.Process.Exit(GitHub.AbstractRootFunctions.deltaExit(Git.Actions.build(GitHub.this))) + Git.this;
